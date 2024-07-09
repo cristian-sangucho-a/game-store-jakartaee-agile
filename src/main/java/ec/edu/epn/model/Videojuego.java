a357dfd0994fb0c0c6827c18af1aeb00010c9ff1
Jean Cotera <jean.cotera@gmail.com>
2024-07-09 18:03:59 -0500

    Modificación: Modifiqué las clases para poder superar las pruebas

--- src/main/java/ec/edu/epn/model/Videojuego.java
+++ src/main/java/ec/edu/epn/model/Videojuego.java
@@ -1,4 +1,64 @@
 package ec.edu.epn.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Videojuego {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+    private double precio;
+    private String Desarrollador;
+
+
+
+
+    public Videojuego(String titulo, String nombreDeDesarrollador, double precio) {
+        this.titulo = titulo;
+        this.Desarrollador = nombreDeDesarrollador;
+        this.precio = precio;
+    }
+
+    public Videojuego() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesarrollador() {
+        return Desarrollador;
+    }
+
+    public void setDesarrollador(String desarrollador) {
+        this.Desarrollador = desarrollador;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id: " + id + '|' +
+                " titulo: " + titulo + '|' +
+                " dev: " + Desarrollador + '|' +
+                " precio: " + precio +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/main/java/ec/edu/epn/model/VideojuegoDAO.java
+++ src/main/java/ec/edu/epn/model/VideojuegoDAO.java
@@ -1,21 +1,93 @@
 package ec.edu.epn.model;
 
+import ec.edu.epn.services.ManejoEntidadPersistencia;
+import ec.edu.epn.model.Videojuego;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+
 import java.util.List;
 
 public class VideojuegoDAO {
-    public List<Videojuego> obtenerVideojuegoPorTitulo(String tituloEntrante) {
-        return null;
+    private String titulo;
+    private String nombreDeDesarrollador;
+    private double precio;
+
+    public VideojuegoDAO(String titulo, String nombreDeDesarrollador, double precio) {
+        this.titulo = titulo;
+        this.nombreDeDesarrollador = nombreDeDesarrollador;
+        this.precio = precio;
     }
 
-    public List<Videojuego> obtenerVideojuegosPorRangoDePrecio(double precioMinimo, double precioMaximo) {
-        return null;
+    public VideojuegoDAO() {
+
     }
 
-    public List<Videojuego> obtenerVideojuegoPorDesarrollador(String nombreDesarrollador) {
-        return null;
+
+    public void crearVideojuego(Videojuego videojuegoAPersistir){
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(videojuegoAPersistir);
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     public List<Videojuego> obtenerTodosLosVideojuego() {
-        return null;
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try{
+            Query query = entityManager.createQuery("SELECT v FROM Videojuego v");
+            return query.getResultList();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public Videojuego obtenerVideojuegoPorId(int IdVideojuego) {
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try{
+            Query query = entityManager.createQuery("SELECT v FROM Videojuego v WHERE v.id = :IdVideojuego");
+            query.setParameter("IdVideojuego", IdVideojuego);
+            return (Videojuego) query.getResultList().get(0);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Videojuego> obtenerVideojuegoPorTitulo(String tituloDelVideojuego) {
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try{
+            Query query = entityManager.createQuery("SELECT v FROM Videojuego v WHERE v.titulo = :tituloDelVideojuego");
+            query.setParameter("tituloDelVideojuego", tituloDelVideojuego);
+            return query.getResultList();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Videojuego> obtenerVideojuegosPorRangoDePrecio(double precioMinimo, double precioMaximo) {
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try{
+            Query query = entityManager.createQuery("SELECT v from Videojuego v where v.precio between :precioMinimo and :precioMaximo");
+            query.setParameter("precioMinimo", precioMinimo);
+            query.setParameter("precioMaximo", precioMaximo);
+            return query.getResultList();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Videojuego> obtenerVideojuegoPorDesarrollador(String nombreDeDesarrollador) {
+        EntityManager entityManager = ManejoEntidadPersistencia.getEntityManager();
+        try{
+            Query query = entityManager.createQuery("SELECT v FROM Videojuego v WHERE v.Desarrollador = :nombreDeDesarrollador");
+            query.setParameter("nombreDeDesarrollador", nombreDeDesarrollador);
+            return query.getResultList();
+        } finally {
+            entityManager.close();
+        }
     }
 }
