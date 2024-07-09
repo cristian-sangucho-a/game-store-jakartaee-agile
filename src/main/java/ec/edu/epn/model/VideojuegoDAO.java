8465b68be13b5e474b6375c6bd717b96a152f94d
Jean Cotera <jean.cotera@gmail.com>
2024-07-09 17:15:24 -0500

    Creacion: TDD para la clase VideojuegoDAO

---
+++ src/main/java/ec/edu/epn/model/VideojuegoDAO.java
@@ -0,0 +1,21 @@
+package ec.edu.epn.model;
+
+import java.util.List;
+
+public class VideojuegoDAO {
+    public List<Videojuego> obtenerVideojuegoPorTitulo(String tituloEntrante) {
+        return null;
+    }
+
+    public List<Videojuego> obtenerVideojuegosPorRangoDePrecio(double precioMinimo, double precioMaximo) {
+        return null;
+    }
+
+    public List<Videojuego> obtenerVideojuegoPorDesarrollador(String nombreDesarrollador) {
+        return null;
+    }
+
+    public List<Videojuego> obtenerTodosLosVideojuego() {
+        return null;
+    }
+}
---
+++ src/test/java/ec/edu/epn/modelo/test/VideojuegoTest.java
@@ -0,0 +1,74 @@
+package ec.edu.epn.modelo.test;
+
+import ec.edu.epn.model.Videojuego;
+import ec.edu.epn.model.VideojuegoDAO;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class VideojuegoTest {
+
+public static VideojuegoDAO videojuegoDAO;
+    @Test
+    public void given_titulo_when_usuario_busque_then_obtener_coincidencias() {
+        // Crear una lista de videojuegos para simular lo que retornaría la base de datos
+        String tituloEntrante = "Elden Ring";
+        List<Videojuego> resultado =videojuegoDAO.obtenerVideojuegoPorTitulo(tituloEntrante);
+        assertNotNull(resultado);
+    }
+
+    @Test
+    public void given_titulo_when_usuario_busque_then_no_obtener_coincidencias(){
+       String tituloEntrante = "Batalla Naval Simon Bolivar";
+        //no se obtiene ninguna coincidencia
+        List<Videojuego> resultado =videojuegoDAO.obtenerVideojuegoPorTitulo(tituloEntrante);
+        assertTrue(resultado.isEmpty());
+
+    }
+
+    @Test
+    public void given_rango_precio_when_usuario_busque_then_obtener_coincidencias(){
+        double precioMinimo = 15.00;
+        double precioMaximo = 25.00;
+        List<Videojuego> resultado =videojuegoDAO.obtenerVideojuegosPorRangoDePrecio(precioMinimo, precioMaximo);
+        assertNotNull(resultado);
+    }
+
+    @Test
+    public void given_rango_precio_when_usuario_busque_then_no_obtener_coincidencias(){
+       double precioMinimo = 155.00;
+       double precioMaximo = 205.00;
+        List<Videojuego> resultado =videojuegoDAO.obtenerVideojuegosPorRangoDePrecio(precioMinimo, precioMaximo);
+       assertTrue(resultado.isEmpty());
+    }
+    @Test
+    public void given_desarrollador_when_usuario_busque_then_obtener_coincidencias() {
+        // Crear una lista de videojuegos para simular lo que retornaría la base de datos
+        String nombreDesarrollador = "FromSoftware";
+        List<Videojuego> resultado =videojuegoDAO.obtenerVideojuegoPorDesarrollador(nombreDesarrollador);
+        assertFalse(resultado.isEmpty());
+    }
+
+    @Test
+    public void given_desarrollador_when_usuario_busque_then_no_obtener_coincidencias(){
+       String nombreDesarrollador = "Cristian Zambrano";
+
+       List<Videojuego> resultado = videojuegoDAO.obtenerVideojuegoPorDesarrollador(nombreDesarrollador);
+       assertTrue(resultado.isEmpty());
+    }
+
+    @Test
+    public void given_inicio_when_ver_catalogo_then_mostrar_catalogo_videojuego(){
+       List<Videojuego> videojuegos = new ArrayList<>();
+       List<Videojuego> catalogo = videojuegoDAO.obtenerTodosLosVideojuego();
+       assertFalse(catalogo.isEmpty());
+    }
+
+}
