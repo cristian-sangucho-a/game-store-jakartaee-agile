587cc2f153ba1424073a7af6773823a9ff7a53ad
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-07-14 16:35:15 -0500

    test: agregar al carrito PBI-59 TASK-63

--- src/test/java/ec/edu/epn/modelo/test/CarritoDeComprasTest.java
+++ src/test/java/ec/edu/epn/modelo/test/CarritoDeComprasTest.java
@@ -7,6 +7,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CarritoDeComprasTest {
     public static CarritoDeCompras carritoDeCompras;
@@ -23,4 +24,15 @@ public class CarritoDeComprasTest {
         carritoDeCompras.quitarVideojuegoDelCarrito(videojuegoNuevo);
         assertFalse(carritoDeCompras.getVideojuegosDelCarrito().containsValue(videojuegoNuevo));
     }
+
+    @Test
+    public void given_cliente_en_carrito_when_agregue_juego_then_el_videojuego_estara_en_el_carrito(){
+        Videojuego videojuegoAAgregar = new Videojuego();
+        this.carritoDeCompras.agregarVideojuegoAlCarrito(videojuegoAAgregar);
+        assertTrue(this.carritoDeCompras.getVideojuegosDelCarrito().containsKey(videojuegoAAgregar));
+    }
+
+
+
+
 }
