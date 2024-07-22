3cbe7e99eadd50ec730170dc66f9933680d8eb95
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-07-22 17:44:34 -0500

    Agregado

--- src/main/java/ec/edu/epn/model/logic/TarjetaDeCredito.java
+++ src/main/java/ec/edu/epn/model/logic/TarjetaDeCredito.java
@@ -4,6 +4,7 @@ public class TarjetaDeCredito {
 
 
     public static boolean validarTarjeta(char[] numeroDeLaTarjeta, String fechaExpiracion, String cvv) {
+        //Implementacion
         return true;
     }
 }
