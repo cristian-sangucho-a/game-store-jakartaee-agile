113cd9e238f5fad67e265fbfab68e11bb90d8070
Jean Cotera <jean.cotera@gmail.com>
2024-07-22 18:20:47 -0500

    Add: Test para la lógica de las tarjetas de crédito

--- src/test/java/ec/edu/epn/modelo/test/TarjetaDeCreditoTest.java
+++ src/test/java/ec/edu/epn/modelo/test/TarjetaDeCreditoTest.java
@@ -1,2 +1,29 @@
-package ec.edu.epn.modelo.test;public class TarjetaDeCreditoTest {
+package ec.edu.epn.modelo.test;
+
+import ec.edu.epn.model.logic.TarjetaDeCredito;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import java.lang.String;
+
+
+public class TarjetaDeCreditoTest {
+    @Test
+    public void given_tarjeta_fue_ingresado_when_quiera_pagar_then_aceptara_la_tarjeta() {
+        String numeroTarjeta = "4857178587547167";
+        String fechaExpiracion = "10/2026";
+        String cvv = "967";
+
+        assertTrue(TarjetaDeCredito.validarTarjeta(numeroTarjeta.toCharArray(), fechaExpiracion, cvv));
+    }
+
+    @Test
+    public void given_tarjeta_fue_ingresada_incorrectamente_when_quiera_pagar_then_rechazara_la_tarjeta() {
+        String numeroTarjeta = "4857178587";
+        String fechaExpiracion = "10/2026";
+        String cvv = "967";
+
+        assertFalse(TarjetaDeCredito.validarTarjeta(numeroTarjeta.toCharArray(), fechaExpiracion, cvv));
+    }
+
 }
