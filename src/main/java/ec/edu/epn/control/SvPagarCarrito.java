07a0937b502a1240464c4cd1bd74005e01010c34
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-07-23 20:04:56 -0500

    fix:  redireccion a la seccion de pagos

--- src/main/java/ec/edu/epn/control/SvPagarCarrito.java
+++ src/main/java/ec/edu/epn/control/SvPagarCarrito.java
@@ -30,9 +30,7 @@ public class SvPagarCarrito  extends HttpServlet {
     private static void pagar(HttpServletRequest request, HttpServletResponse response, String numeroTarjeta, String fechaExpiracion, String cvv, String titularDeLaTarjeta) throws IOException {
         ValidarTarjeta  validarTarjeta = new ValidarTarjeta();
         if(!validarTarjeta.validarTarjeta(numeroTarjeta.toCharArray(), fechaExpiracion, cvv)){
-            response.setContentType("text/html");
-            response.getWriter().print("<html><body><script>alert('¡Pago invalido!');</script></body></html>");
-            response.sendRedirect("index.jsp");
+            response.sendRedirect("seccionPagos.jsp");
             return;
         }
         PagoDAO pagoDAO = new PagoDAO();
