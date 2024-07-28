79167d31076da67732b65c2afb16b8df55fe8340
unknown <cristian.zambrano@epn.edu.ec>
2024-07-27 19:01:06 -0500

    Feat: clase pojo de cliente creada

---
+++ src/main/java/ec/edu/epn/model/entities/Cliente.java
@@ -0,0 +1,77 @@
+package ec.edu.epn.model.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasenia;
+    private boolean esAdmin;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nombre,  String apellido, String correo, String contrasenia, boolean esAdmin) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.esAdmin = esAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isEsAdmin() {
+        return esAdmin;
+    }
+
+    public void setEsAdmin(boolean esAdmin) {
+        this.esAdmin = esAdmin;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
