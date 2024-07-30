d030b80454b6d2d36eb6565c776d5a0c43452204
Jean Cotera <jean.cotera@gmail.com>
2024-07-30 17:56:34 -0500

    Fix: Clase Biblioteca nueva columna para que existan datos que persistir

--- src/main/java/ec/edu/epn/model/entities/Biblioteca.java
+++ src/main/java/ec/edu/epn/model/entities/Biblioteca.java
@@ -3,6 +3,7 @@ package ec.edu.epn.model.entities;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
@@ -10,13 +11,20 @@ public class Biblioteca {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
+    private String nombre;
     @ManyToMany
     @JoinTable(
             name = "biblioteca_videojuego", // Join table name
             joinColumns = @JoinColumn(name = "biblioteca_id"), // Foreign key for Biblioteca
             inverseJoinColumns = @JoinColumn(name = "videojuego_id") // Foreign key for Videojuego
     )
-    private ArrayList<Videojuego> videojuegos;
+    private List<Videojuego> videojuegos;
+
+    public Biblioteca(String nombre) {
+        videojuegos = new ArrayList<>();
+        this.nombre = nombre;
+    }
 
     public Biblioteca() {
         videojuegos = new ArrayList<>();
@@ -30,7 +38,7 @@ public class Biblioteca {
         this.id = id;
     }
 
-    public ArrayList<Videojuego> getVideojuegos() {
+    public List<Videojuego> getVideojuegos() {
         return videojuegos;
     }
 
